e40b9164d1bec11291e7550c93e0dd17ac2e6083
Amanda <amandadionizio04@gmail.com>
2025-11-06 08:57:22 -0300

    mensagem

---
+++ arquivo.py
@@ -0,0 +1,5 @@
+def saudacao(nome):
+    """Retorna uma saudação personalizada."""
+    return f"olá, {nome}! Este e um teste de integração VS code + GitHub."
+mensagem = saudacao("Mundo GitHub")
+print(mensagem)
