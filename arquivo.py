8925d13c92e8dbd1bb7241f093d9edd1792872d2
Amanda <amandadionizio04@gmail.com>
2025-11-06 09:40:53 -0300

    input adicionado

--- arquivo.py
+++ arquivo.py
@@ -3,3 +3,6 @@ def saudacao(nome):
     return f"olá, {nome}! Este e um teste de integração VS code + GitHub."
 mensagem = saudacao("Mundo GitHub")
 print(mensagem)
+
+nome = input("Digite seu nome: ")
+print("Olá seja muito bem vindo,", nome)
