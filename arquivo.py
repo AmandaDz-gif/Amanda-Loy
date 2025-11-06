2d0b91e0f6a58618ba8162e827d3bed2a9641713
Lauring Phedia SaintHubert <lauringsainthubert@gmail.com>
2025-11-06 09:25:29 -0300

    mensagem

--- arquivo.py
+++ arquivo.py
@@ -3,3 +3,9 @@ def saudacao(nome):
     return f"olá, {nome}! Este e um teste de integração VS code + GitHub."
 mensagem = saudacao("Mundo GitHub")
 print(mensagem)
+
+def bemvinda(nome):
+    """Retorna bemvindo."""
+    return f"ola, {nome}! este e um teste de integração VS code + GitHub."
+    mensagem = bemvindo("Mundo GitHub")
+    print(mensagem)
